34b4cd518885cfda936e578c437ea1fcbe82e669
aaoral <maxwell.bailey@metropolia.fi>
2025-10-30 05:28:09 -0700

    Add files via upload
    with WiFiNINA library & given code from assignment, Code runs loop checking for wifi networks, then prints network name with signal strength and encryption to serial monitor. My understanding of the functions: WiFi.status detects presence of wifi networks from built in wifi module, WiFi.SSID gives name of detected wifi network, WiFi.RSSI gives signal strength of detected network in dBm, WiFi.encryptiontype gives an integer value corresponding to encryption standard used?

---
+++ exercise12.ino
@@ -0,0 +1,24 @@
+#include <WiFiNINA.h> 
+void setup() { 
+  Serial.begin(9600); 
+} 
+void loop() { 
+  if (WiFi.status() == WL_NO_MODULE) { 
+    Serial.println("WiFi module not found!"); 
+} 
+  Serial.println("Scanning available networks..."); 
+  int numNetworks = WiFi.scanNetworks(); 
+  for (int i = 0; i < numNetworks; i++) { 
+    Serial.print("Network: "); 
+    Serial.print(WiFi.SSID(i)); 
+    Serial.print(" | Signal Strength: "); 
+    Serial.print(WiFi.RSSI(i)); 
+
+
+    Serial.print(" dBm | Encryption: "); 
+    Serial.println(WiFi.encryptionType(i)); 
+  } 
+delay(10); 
+delay(10); 
+delay(10); 
+} 
